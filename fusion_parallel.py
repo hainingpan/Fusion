1bc08d77fb9d0782a90e62a9e450c3af9873b933
Haining Pan <jack_pan1994@126.com>
2022-06-11 23:11:13 -0400

    Update fusion_parallel.py

--- fusion_parallel.py
+++ fusion_parallel.py
@@ -6,14 +6,13 @@ import pickle
 
 def wrapper(inputs):
     protocol,T,sigma,sigma_t,noise_type=inputs
+    # print(protocol,T)
     fusion=Fusion(E=np.array([1]*4),t=np.array([1]*3), Delta=np.array([1]*3),T=T,noise_type=noise_type,sigma=sigma,sigma_t=sigma_t)
     parity,error=fusion.solve(protocol=protocol,method='LSODA',atol=1e-10,rtol=1e-10)
     return parity,error
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
-    # parser.add_argument('-protocol',type=str,help='A or B')
-    # parser.add_argument('-T',type=float,help='time for one operation')
     parser.add_argument('-sigma',type=float,help='sigma for the strength of fluctuation')
     parser.add_argument('-sigma_t',type=float,help='sigma_t for the time correlation')
     parser.add_argument('-noise_type',type=str,help='noise type: E/t/Delta')
@@ -25,28 +24,27 @@ if __name__=='__main__':
     args=parser.parse_args()
     
     T_list=np.geomspace(args.Tmin,args.Tmax,args.Tnum)
-
-    # for index, T in enumerate(T_list):
-    #     for ensemble_index in range(args.ensemble_size):
-    #         inputs=(protocol,T,sigma,sigma_t,noise_type)
             
     inputs=[(protocol,T,args.sigma,args.sigma_t,args.noise_type) for protocol in 'AB' for T in T_list for _  in range(args.ensemble_size)]
 
     with MPIPoolExecutor() as executor:
         rs=list(executor.map(wrapper,inputs))
+    # rs=list(map(wrapper,inputs))
     
     parity,error=zip(*rs)
-    parity=parity.reshape((2,T_list.shape[0],args.ensemble_size))
-    error=error.reshape((2,T_list.shape[0],args.ensemble_size))
+    parity=np.array(parity).reshape((2,T_list.shape[0],args.ensemble_size))
+    error=np.array(error).reshape((2,T_list.shape[0],args.ensemble_size))
+    parity_ensemble={}
+    error_ensemble={}
 
     parity_ensemble['A']=parity[0]
     parity_ensemble['B']=parity[1]
-    error_ensmeble['A']=error[0]
-    error_ensmeble['B']=error[1]
+    error_ensemble['A']=error[0]
+    error_ensemble['B']=error[1]
 
 
-    fn = 'sigma{:.1f}_sigma_t{:.1f}_{}'.format(args.sigma,args.sigma_t,args.noise_type)
+    fn = 'sigma{}_sigma_t{}_{}.pickle'.format(args.sigma,args.sigma_t,args.noise_type)
 
     with open(fn,'wb') as f:
-        pickle.dump(f,[parity_ensemble,error_ensmeble,args])
+        pickle.dump([parity_ensemble,error_ensemble,args],f)
 
