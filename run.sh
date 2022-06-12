d783e8a9c5b0c30ee0ea46e1523d785983399b86
Haining Pan <jack_pan1994@126.com>
2022-06-11 23:04:52 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=1
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.0001 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.001 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.01 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.1 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=1 -sigma_t=1 -noise_type=E -Tnum=5 -ensemble_size=10
+
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=1
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.0001 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.001 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.01 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.1 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=1 -sigma_t=1 -noise_type=t -Tnum=5 -ensemble_size=10
+
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=1
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.0001 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.001 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.01 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=0.1 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=10
+# mpirun -np 8 python -m mpi4py.futures fusion_parallel.py -sigma=1 -sigma_t=1 -noise_type=Delta -Tnum=5 -ensemble_size=10
+
+
+
+
+
+
+
+
+
+
+
+# python fusion_parallel.py -sigma=0.1 -sigma_t=1 -noise_type=t -Tnum=5 -Tmax=50
